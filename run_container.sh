b79f4fe3822a56681fc6f71cd06feb9cdc76f108
Marynel Vazquez <marynel.vazquez@yale.edu>
2026-04-07 11:39:30 -0400

    updated script to run container so that we can now show the app outside of docker

--- run_container.sh
+++ run_container.sh
@@ -1,13 +1,23 @@
 # Run container with specified image name and interactive terminal
-# usage: run_container.sh <image_name> (e.g., run_container.sh react-env:latest)
+# usage: run_container.sh <image_name> <directory-to-bind> <port-to-bind> (e.g., run_container.sh react-env:latest)
 #!/bin/bash
-set -e
+set -e # exit if any error comes up!
 
 if [ -z "$1" ]; then
-  echo "Usage: $0 <image_name>"
+  echo "Usage: $0 <image_name> <directory-to-bind> <port-to-bind>"
   exit 1
 fi
 IMAGE_NAME=$1
+
+# optional arguments
+BIND_DIRECTORY=$2
+PORT_TO_BIND=$3
+
+# set default port to bind to 5173 if not provided
+if [ -z "$PORT_TO_BIND" ]; then
+  PORT_TO_BIND=5173
+fi
+
 
 # Export DISPLAY so GUI applications inside the container can show on the host.
 # Docker Desktop on macOS does not support --net=host, so we use host.docker.internal instead.
@@ -22,6 +32,17 @@ else
     xhost +local:docker
     MYDISPLAY=${DISPLAY:-:0}
     DOCKER_ARGS=(--rm -it --net=host -e DISPLAY="$MYDISPLAY" -v /tmp/.X11-unix:/tmp/.X11-unix)
+fi
+
+# check if the bind directory exists
+if [ ! -d "$BIND_DIRECTORY" ]; then
+  echo "Error: Directory '$BIND_DIRECTORY' does not exist."
+  exit 1
+else
+  # add the bind directory to the docker arguments
+  # and expose port 5173 for Vite development server 
+  # once the container is running with "npm run dev -- --host 0.0.0.0", you can access the Vite server at http://127.0.0.1:5173 on your host machine
+  DOCKER_ARGS+=(-v "$BIND_DIRECTORY":/app:rw --workdir /app -p 127.0.0.1:$PORT_TO_BIND:$PORT_TO_BIND)
 fi
 
 echo "Running container with image: $IMAGE_NAME"
